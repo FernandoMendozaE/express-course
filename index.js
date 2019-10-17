a713afa305296b60cb0ec5834b6444d1efa7766b
FerYotta <feryotta2@gmail.com>
2019-10-17 14:11:09 -0400

    Peticiones Request (get, post, put and delete)

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ const express = require("express");
 const app = express(); //obtiene el objeto express
 
 app.get("/", (require, res) => {
-  res.send("Hello World"); //responde la petición
+  res.send("PETICION GET RECEIVED"); //responde la petición
 }); //recibe una peticion HTTP(get) y realiza algo
 
-app.listen(3000, () => {
-  console.log("Serve on port 3000");
+// Creación de Router
+app.post("/about", (require, res) => {
+  res.send("POST REQUEST RECEIVED"); //responde la petición
+});
+
+app.put("/contact", (require, res) => {
+  res.send("UPDATE REQUESR RECEIVED"); //responde la petición
+});
+
+app.delete("/test", (require, res) => {
+  res.send("<h1>DELETE REQUEST RECEIVED</h1>"); //responde la petición
+});
+
+app.listen(5000, () => {
+  console.log("Serve on port 5000");
 }); //levantar el servicio
