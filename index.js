7c57b512b0a9f07f8f668b796c10e13fc1a78f18
FerYotta <feryotta2@gmail.com>
2019-10-18 09:01:20 -0400

    Examples request (get, post, put, delete)

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 const express = require("express");
 const app = express(); //obtiene el objeto express
 
-app.get("/", (require, res) => {
-  res.send("PETICION GET RECEIVED"); //responde la petición
+app.use(express.json()); // linea de código encargado de hacer conocer el formato JSON
+
+app.all("/user", (req, res, next) => {
+  console.log("Por aqui paso");
+  next();
+}); // método express encargado de pasen toda las ruras (/user)
+
+app.get("/user", (req, res) => {
+  res.json({
+    username: "Cameron",
+    lastname: "Howe"
+  }); //responde la petición
 }); //recibe una peticion HTTP(get) y realiza algo
 
 // Creación de Router
-app.post("/about", (require, res) => {
-  res.send("POST REQUEST RECEIVED"); //responde la petición
+//:id : request parms (parametro recivido mediante la url)
+app.post("/user/:id", (req, res) => {
+  console.log(req.body); //obtiene datos del rquest json
+  console.log(req.params); //obtiene  datos del id (url) parametros
+  res.send("POST REQUEST RECEIVED"); //request post
 });
 
-app.put("/contact", (require, res) => {
-  res.send("UPDATE REQUESR RECEIVED"); //responde la petición
+app.put("/user/:userId", (req, res) => {
+  console.log(req.body);
+  res.send(`User ${req.params.userId} update`);
 });
 
-app.delete("/test", (require, res) => {
-  res.send("<h1>DELETE REQUEST RECEIVED</h1>"); //responde la petición
+app.delete("/user/:id", (req, res) => {
+  res.send(`User ${req.params.id} deleted`); //responde la petición
 });
 
 app.listen(5000, () => {
